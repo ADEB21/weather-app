9b76f190441a7be74eb6eb74478f9f09f8cbb5ad
ADEB <arthurdebruille@proton.me>
2026-02-10 19:09:24 +0100

    get more data

--- src/Controller/Api/WeatherController.php
+++ src/Controller/Api/WeatherController.php
@@ -5,26 +5,15 @@ namespace App\Controller\Api;
 use App\Service\OpenMeteoClient;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/api/weather', name: 'api_weather')]
+#[Route('/api/weather')]
 class WeatherController extends AbstractController
 {
     #[Route('', methods: ['GET'])]
-    public function __invoke(Request $request, OpenMeteoClient $client): JsonResponse
+    public function __invoke(OpenMeteoClient $client): JsonResponse
     {
-        $latitude = $request->query->get('latitude');
-        $longitude = $request->query->get('longitude');
-
-        if ($latitude === null || $longitude === null) {
-            return $this->json([
-                'error' => 'latitude and longitude parameters are required'
-            ], Response::HTTP_BAD_REQUEST);
-        }
-
-        $data = $client->getWeather((float) $latitude, (float) $longitude);
+        $data = $client->getWeather(47.3220, 5.0415);
 
         return $this->json($data);
     }
